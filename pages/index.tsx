63f9f41d8e378136fe5cf8d31a762e86aa6f8e46
koohoyn <koohoyn@naver.com>
2022-04-16 19:18:36 +0900

    Fix/main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,41 +1,51 @@
-import type { NextPage } from "next";
+import React from "react";
 import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+import styles from "../styles/Home.module.css";
 
-const Home: NextPage = () => {
+export default function App() {
+  const alignCenter = { display: "flex", alignItems: "center" };
   return (
-    <Parallax pages={2} style={{ top: "0", left: "0" }}>
-      <ParallaxLayer
-        offset={0}
-        speed={2.5}
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <p>Scroll down</p>
-      </ParallaxLayer>
+    <div>
+      <div className={styles.background} />
 
-      <ParallaxLayer
-        offset={1}
-        speed={2}
-        style={{ backgroundColor: "#ff6d6d" }}
-      />
+      <Parallax pages={5}>
+        <ParallaxLayer
+          offset={0}
+          speed={0.5}
+          style={{ ...alignCenter, justifyContent: "center" }}
+        >
+          <p className={styles.scrollText}>Scroll down</p>
+        </ParallaxLayer>
 
-      <ParallaxLayer
-        offset={1}
-        speed={0.5}
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          color: "white",
-        }}
-      >
-        <p>Scroll up</p>
-      </ParallaxLayer>
-    </Parallax>
+        <ParallaxLayer
+          sticky={{ start: 1, end: 3 }}
+          style={{ ...alignCenter, justifyContent: "flex-start" }}
+        >
+          <div className={`${styles.card} ${styles.sticky}`}>
+            <p>I'm a sticky layer</p>
+          </div>
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={1.5}
+          speed={1.5}
+          style={{ ...alignCenter, justifyContent: "flex-end" }}
+        >
+          <div className={`${styles.card} ${styles.parallax} ${styles.purple}`}>
+            <p>I'm not</p>
+          </div>
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={2.5}
+          speed={1.5}
+          style={{ ...alignCenter, justifyContent: "flex-end" }}
+        >
+          <div className={`${styles.card} ${styles.parallax} ${styles.blue}`}>
+            <p>Neither am I</p>
+          </div>
+        </ParallaxLayer>
+      </Parallax>
+    </div>
   );
-};
-
-export default Home;
+}
